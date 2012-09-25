631f39a84df9ab6c19276a1d09ef3dec9165bb99
danielbpdias <danielbpdias@gmail.com>
2012-09-25 13:09:03 +0000

    Added generator to TriangleTireWorld domain

--- src/mdp/CallPrincipal.java
+++ src/mdp/CallPrincipal.java
@@ -1,8 +1,6 @@
 package mdp;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import generator.TriangleTireWorldGen;
 
 public class CallPrincipal {
 
@@ -11,128 +9,18 @@ public class CallPrincipal {
 	 */
 	
 	public static void main(String args[])
-	   {
-	      Process theProcess = null;
-	      BufferedReader inStream = null;
-	      
-	      for(int numComp=3; numComp<=7; numComp++){
-	    	 /* for (int prec=0;prec<=50;prec=prec+10){
-	    		  float precision=(float)numComp*prec/100;
-	    		  String prog="java mdp.Principal /home/karina/ADDVer2/ADD/problemsMDPIP/uni_ring_IP_"+numComp+".net 50 "+precision+" 1  /home/karina/ADDVer2/ADD/reportsMDPIP/ADDResults.tex 0";
-	    		  System.out.println("calling "+prog);
-	    		  try
-	    		  {
-
-	    			  theProcess = Runtime.getRuntime().exec(prog);
-	    			  theProcess.waitFor();
-	    			  System.out.println("Finish ADD");
-	    		  }catch (InterruptedException ie) {
-	    			  System.out.println(ie);
-	    			  ie.printStackTrace();
-	    		  }catch(IOException e)
-	    		  {
-	    			  System.err.println("Error on exec() method");
-	    			  e.printStackTrace();  
-	    		  }
-	    		  // read from the called program's standard output stream
-	    		  try
-	    		  {
-	    			  inStream = new BufferedReader(new InputStreamReader( theProcess.getInputStream() ));  
-	    			  System.out.println(inStream.readLine());
-	    		  }
-	    		  catch(IOException e)
-	    		  {
-	    			  System.err.println("Error on inStream.readLine()");
-	    			  e.printStackTrace();  
-	    		  }
-
-	    	  }*/
-	    	  Process theProcess1 = null;
-	    	  String progTable="java mdp.Principal /home/karina/ADDVer2/ADD/problemsMDPIP/uni_ring_IP_"+numComp+".net 50 0 3  /home/karina/ADDVer2/ADD/reportsMDPIP/TableResults.tex 0 Flat";
-    		  System.out.println("calling "+progTable);
-    		  try
-    		  {
-
-    			  theProcess1= Runtime.getRuntime().exec(progTable);
-    			  theProcess1.waitFor();
-    			  System.out.println("Finish Table");
-    		  }catch (InterruptedException ie) {
-    			  System.out.println(ie);
-    			  ie.printStackTrace();
-    		  }catch(IOException e)
-    		  {
-    			  System.err.println("Error on exec() method");
-    			  e.printStackTrace();  
-    		  }
-    		  // read from the called program's standard output stream
-    		  try
-    		  {
-    			  inStream = new BufferedReader(new InputStreamReader( theProcess1.getInputStream() ));  
-    			  System.out.println(inStream.readLine());
-    		  }
-    		  catch(IOException e)
-    		  {
-    			  System.err.println("Error on inStream.readLine()");
-    			  e.printStackTrace();  
-    		  }
-	    	  
-        	  Process theProcess2 = null;
-	    	  String progTable2="java mdp.Principal /home/karina/ADDVer2/ADD/problemsMDPIP/bi_ring_IP_"+numComp+".net 50 0 3  /home/karina/ADDVer2/ADD/reportsMDPIP/TableResults.tex 0 Flat";
-    		  System.out.println("calling "+progTable2);
-    		  try
-    		  {
-
-    			  theProcess2= Runtime.getRuntime().exec(progTable2);
-    			  theProcess2.waitFor();
-    			  System.out.println("Finish Table");
-    		  }catch (InterruptedException ie) {
-    			  System.out.println(ie);
-    			  ie.printStackTrace();
-    		  }catch(IOException e)
-    		  {
-    			  System.err.println("Error on exec() method");
-    			  e.printStackTrace();  
-    		  }
-    		  // read from the called program's standard output stream
-    		  try
-    		  {
-    			  inStream = new BufferedReader(new InputStreamReader( theProcess2.getInputStream() ));  
-    			  System.out.println(inStream.readLine());
-    		  }
-    		  catch(IOException e)
-    		  {
-    			  System.err.println("Error on inStream.readLine()");
-    			  e.printStackTrace();  
-    		  }
-         	  Process theProcess3 = null;
-	    	  String progTable3="java mdp.Principal /home/karina/ADDVer2/ADD/problemsMDPIP/indep_ring_IP_"+numComp+".net 50 0 3  /home/karina/ADDVer2/ADD/reportsMDPIP/TableResults.tex 0 Flat";
-    		  System.out.println("calling "+progTable3);
-    		  try
-    		  {
-
-    			  theProcess3= Runtime.getRuntime().exec(progTable3);
-    			  theProcess3.waitFor();
-    			  System.out.println("Finish Table");
-    		  }catch (InterruptedException ie) {
-    			  System.out.println(ie);
-    			  ie.printStackTrace();
-    		  }catch(IOException e)
-    		  {
-    			  System.err.println("Error on exec() method");
-    			  e.printStackTrace();  
-    		  }
-    		  // read from the called program's standard output stream
-    		  try
-    		  {
-    			  inStream = new BufferedReader(new InputStreamReader( theProcess3.getInputStream() ));  
-    			  System.out.println(inStream.readLine());
-    		  }
-    		  catch(IOException e)
-    		  {
-    			  System.err.println("Error on inStream.readLine()");
-    			  e.printStackTrace();  
-    		  }	  
-	    	  
-	      }
-	   }
+	{
+		for (int i = 0; i < 5; i++) {
+			int numberOfVariables = 2 + (1 + (i + 3)) * (i + 3) / 2; //2 + arithmethic progression sum from 1 to i, steps 1
+			
+			String[] anotherArgs = {
+				"//home//daniel//workspaces//java//mdpip//ADD//problemsMDPIP//tests//triangle_tireworld_" + numberOfVariables  + ".net",
+				Integer.toString(i+1),
+				"0.9",
+				"0.01"
+			};
+			
+			TriangleTireWorldGen.main(anotherArgs);
+		}		
+	}
 }
--- src/util/Pair.java
+++ src/util/Pair.java
@@ -14,13 +14,6 @@
 // Package definition
 package util;
 
-// Packages to import
-import java.io.*;
-import java.math.*;
-import java.util.*;
-
-import graph.*;
-
 /**
  * @version   1.0
  * @author    Scott Sanner
@@ -32,36 +25,45 @@ public class Pair implements Comparable {
     Object _o2;
 
     public Pair(Object o1, Object o2) {
-	_o1 = o1;
-	_o2 = o2;
+		_o1 = o1;
+		_o2 = o2;
     }
 
-    public int hashCode() {
-	return _o1.hashCode() - _o2.hashCode();
+    public Object get_o1() {
+		return _o1;
+	}
+
+	public Object get_o2() {
+		return _o2;
+	}
+
+	public int hashCode() {
+    	return _o1.hashCode() - _o2.hashCode();
     }
 
     public boolean equals(Object o) {
-	if (o instanceof Pair) {
-	    Pair p = (Pair)o;
-	    return (_o1.equals(p._o1) && _o2.equals(p._o2));
-	} else {
-	    return false;
-	}
+		if (o instanceof Pair) {
+		    Pair p = (Pair)o;
+		    return (_o1.equals(p._o1) && _o2.equals(p._o2));
+		} else {
+		    return false;
+		}
     }
     
     // Perform an ordered comparison, o1's then o2's if o1's are equal
     public int compareTo(Object o) {
+		Pair p = (Pair)o;
+		Comparable c1   = (Comparable)_o1;
+		Comparable p_c1 = (Comparable)p._o1;
 
-	Pair p = (Pair)o;
-	Comparable c1   = (Comparable)_o1;
-	Comparable p_c1 = (Comparable)p._o1;
-	int comp_o1 = c1.compareTo(p_c1);
-	if (comp_o1 != 0) {
-	    return comp_o1;
-	} 
-	
-	Comparable c2   = (Comparable)_o2;
-	Comparable p_c2 = (Comparable)p._o2;
-	return c2.compareTo(p_c2);
+		int comp_o1 = c1.compareTo(p_c1);
+		
+		if (comp_o1 != 0) {
+		    return comp_o1;
+		} 
+		
+		Comparable c2   = (Comparable)_o2;
+		Comparable p_c2 = (Comparable)p._o2;
+		return c2.compareTo(p_c2);
     }
 }
